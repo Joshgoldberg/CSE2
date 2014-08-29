5f11a8a2b53da28de6405f51d17aadfd38d56965
joshgoldberg <jag317@lehigh.edu>
2014-08-29 13:34:41 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+//////////
+//first homework, just printing
+
+public class  WelcomeClass{
+    //
+    public static void main(String[ ] args) {
+        
+        //inner text
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println(" /\\ /\\ /\\ /\\ /\\ /\\ ");
+        System.out.println("|-J--A--G--3--1--7-|");
+        System.out.println(" \\/ \\/ \\/ \\/ \\/ \\/ ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("My friend and I stayed up for 2 days straight finishing a song we were producing");
+        
+    }
+}
