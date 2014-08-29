a3803561799a036ab120fd1a98dff552204142d2
joshgoldberg <jag317@lehigh.edu>
2014-08-29 00:11:28 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+////////////////////
+//Josh Goldberg
+//Demo 0
+//Hello World Java Program
+//
+// Compiling and programing
+
+//Defining the Class
+
+public class HelloWorld {
+    //adding method
+    public static void main(String[ ] args) {
+        
+        //inner text
+        System.out.println("############################");
+        System.out.println("############################");
+        System.out.println("#                          #");
+        System.out.println("#    HellowWorld 2014!!    #");
+        System.out.println("#                          #");
+        System.out.println("############################");
+        System.out.println("############################");
+        System.out.println("############################");
+        System.out.println("test");
+        
+    }
+}
