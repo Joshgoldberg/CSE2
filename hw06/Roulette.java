5e8c6e910b00986b574c37f7e8db64c1426f03eb
joshgoldberg <jag317@lehigh.edu>
2014-10-15 21:31:45 +0000

    issue with cloud 9 didnt add comments

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -1,55 +1,56 @@
 //roulette java
-//figuring out 
+//figuring out odds of winning with a stratagey
+//
     public class Roulette{
     			// main
    			public static void main(String[] args) {
-   			    int winnings=0;
-   			    int f=0;
-   			    int will=0;
+   			    int winnings=0;//for figuring out times winnings profit
+   			    int f=0;//for 1000 times loop
+   			    int will=0;//total winnings outside loop
    			
-   			    int ddd= 0;
-   			    int internal =0;
    			    
-   			  while(f<1000)  {
+   			    
+   			    
+   			  while(f<1000)  {// outter loop for 1000 simulations
    			     
    			 
-   			     int g=0;
+   			     int g=0;// so we can run it 1000 times
    			    
-   			    int rmynumber=(int)(Math.random()*38);
-   			    int cash=100;
+   			    int rmynumber=(int)(Math.random()*38);//random number instead of input since it doesnt matter
+   			    int cash=100;//starting money
    			    
-       			    while(g<100){
+       			    while(g<100){//inner loop
        			       
-       			   int ryournumber = (int)(Math.random()*38); 
+       			   int ryournumber = (int)(Math.random()*38); //roullete wheel
                   
-       			    if (rmynumber==ryournumber){
-       			      cash=((cash+36));
+       			    if (rmynumber==ryournumber){//for winning
+       			      cash=((cash+36));//moNEY!
        			     
        			    }
-       			    else {
-       			        cash=((cash-1));
+       			    else {//losing!
+       			        cash=((cash-1));//loss
        			        
        			    }   
        			    
        			    
        			    
-       			    g++;
+       			    g++;//restart the oop for 100
                   
        			     
        			    }//while cash to make sure it doesnt go negative
    		         
-   	        	 if (cash>108)
+   	        	 if (cash>108)//this is for if we walk away from the table with a win
                 {winnings++;
-                  will=will+cash; 
+                  will=will+cash; //winnings
                 }
    			 
-   			 f++;
+   			 f++;//run it 100 times
    			  
    			  }   
-   			 double loses = 1000-winnings;
-   			 int lose= (int) loses;
-   			  System.out.println("Total Winnings: "+ will);
+   			 double loses = 1000-winnings;//opisite around winnings
+   			 int lose= (int) loses;// just casue ya need to
+   			  System.out.println("Total Winnings: "+ will);//printting
    			  System.out.println("Total Wins: "+ winnings);
    			  System.out.println("Total Loses: "+ lose);
    			}
-    }
+    }//done
